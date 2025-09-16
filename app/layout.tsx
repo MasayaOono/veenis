d8ad1771e8e9ac2a3b61301dffebd0a4208c1b34
MasayaOono0123 <m.oono@nhsystems.co.jp>
2025-09-16 09:27:23 +0900

    サスペンス

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,7 @@ import Providers from "./providers";
 import Header from "./_components/Header";
 import { Container } from "@mui/material";
 import "./globals.css";
-
+import { Suspense } from "react";
 export const metadata: Metadata = { title: "Veenis" };
 
 export default function RootLayout({
@@ -15,16 +15,18 @@ export default function RootLayout({
   return (
     <html lang="ja">
       <body>
-        <Providers>
-          <Header />
-          <Container
-            maxWidth={false}
-            disableGutters
-            sx={{ py: 3, px: { xs: 2, sm: 3 } }}
-          >
-            {children}
-          </Container>
-        </Providers>
+        <Suspense fallback={null}>
+          <Providers>
+            <Header />
+            <Container
+              maxWidth={false}
+              disableGutters
+              sx={{ py: 3, px: { xs: 2, sm: 3 } }}
+            >
+              {children}
+            </Container>
+          </Providers>
+        </Suspense>
       </body>
     </html>
   );
